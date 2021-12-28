35d01cadf04a6f333848bed0849b79ec10dfa82d
sergey-sanches-peres <sanchesgithub@mail.ru>
2021-12-28 22:59:24 +0300

    comm

--- src/main/java/Classes/Box.java
+++ src/main/java/Classes/Box.java
@@ -9,11 +9,18 @@ public class Box {
 class BoxDemo{
     public static void main(String[] args) {
         Box myBox = new Box();
+        Box myBox2 = new Box();
         double vol;
         myBox.weight = 22;
         myBox.height = 10;
         myBox.depth = 10;
+        double vol2;
+        myBox2.depth = 12;
+        myBox2.height = 15;
+        myBox2.weight = 5;
         vol = myBox.depth * myBox.height * myBox.weight;
         System.out.println("Объем коробки равен: " + vol);
+        vol2 = myBox2.depth * myBox2.height * myBox2.weight;
+        System.out.println("Объем второй коробки равен: " + vol2);
     }
 }
