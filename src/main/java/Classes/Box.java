3709d582dd76ba6f3a41b3f0907ff5023477dc45
sergey-sanches-peres <sanchesgithub@mail.ru>
2021-12-29 16:25:43 +0300

    added volume to the Box class

--- src/main/java/Classes/Box.java
+++ src/main/java/Classes/Box.java
@@ -4,6 +4,11 @@ public class Box {
     double weight;
     double height;
     double depth;
+
+    void parallepipedVolume(){
+        double volume = weight * height * depth;
+        System.out.println("Объем параллепипеда равен: " + volume);
+    }
 }
 
 class BoxDemo{
@@ -22,5 +27,24 @@ class BoxDemo{
         System.out.println("Объем коробки равен: " + vol);
         vol2 = myBox2.depth * myBox2.height * myBox2.weight;
         System.out.println("Объем второй коробки равен: " + vol2);
+    }
+}
+class BoxDemo2{
+    public static void main(String[] args) {
+        Box box1 = new Box();
+        Box box2 = new Box();
+
+        //присваивание значений переменной класса box1
+        box1.weight = 20;
+        box1.depth = 12;
+        box1.height = 10;
+        //присваивание значений переменной класса box2
+        box2.depth = 12;
+        box2.height = 10;
+        box2.weight = 12;
+        //посчитать
+        box1.parallepipedVolume();
+        box2.parallepipedVolume();
+
     }
 }
