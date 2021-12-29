e254d56539797fb811e567b1657cece4555463d0
sergey-sanches-peres <sanchesgithub@mail.ru>
2021-12-29 23:47:26 +0300

    added Overload class

--- src/main/java/Classes/Box.java
+++ src/main/java/Classes/Box.java
@@ -103,3 +103,5 @@ class BoxDemo6{
 }
 
 
+
+
---
+++ src/main/java/Classes/OverLoadDemo.java
@@ -0,0 +1,27 @@
+package Classes;
+
+public class OverLoadDemo {
+
+    void test(){
+        System.out.println("Параметры отсутствуют");
+    }
+    void test(int a){
+        System.out.println("Ваш параметр = " + a);
+    }
+    void test(int a, int b){
+        System.out.println(a + b);
+    }
+    double test(double a){
+        return a + a;
+    }
+}
+class OverLoad{
+    public static void main(String[] args) {
+        OverLoadDemo overLoadDemo = new OverLoadDemo();
+        overLoadDemo.test();
+        overLoadDemo.test(12);
+        overLoadDemo.test(12,34);
+        double res = overLoadDemo.test(12.32);
+        System.out.println(res);
+    }
+}
