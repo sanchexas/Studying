80904064019cb6033d937de11b03ed4f9835499a
sergey-sanches-peres <sanchesgithub@mail.ru>
2021-12-30 15:40:21 +0300

    promote commit

---
+++ src/main/java/pac_1/Class_1.java
@@ -0,0 +1,9 @@
+package pac_1;
+
+public class Class_1 { //продвижение, преобразование типов
+    public static void main(String[] args) {
+        byte b = 50;
+        b = (byte)(b * 2); //преобразование байта в инт
+        System.out.println(b);
+    }
+}
