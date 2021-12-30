be765ef8e99bd00eec62657f5dfa523ddb21b19d
sergey-sanches-peres <sanchesgithub@mail.ru>
2021-12-31 02:39:22 +0300

    arrays training

--- src/main/java/ArraysStuff/task1.java
+++ src/main/java/ArraysStuff/task1.java
@@ -69,3 +69,29 @@ class Task1Practice2{
         System.out.println("Среднее значение = " + avg);
     }
 }
+class Task1Practice3{
+    public static void main(String[] args) {
+        double[] array = new double[5];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.random();
+        }
+
+        double min = array[0];
+        double max = array[0];
+        double avg = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]){
+                min = array[i];
+            }
+            if (max < array[i]){
+                max = array[i];
+            }
+            avg = avg + array[i] / array.length;
+        }
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
+        System.out.println("avg = " + avg);
+    }
+}
