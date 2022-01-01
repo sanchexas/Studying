43523a80c2aeb0278f80096790d0f03812fdd764
sergey-sanches-peres <sanchesgithub@mail.ru>
2022-01-01 17:04:49 +0300

    training

--- src/main/java/pac_1/StringReverse.java
+++ src/main/java/pac_1/StringReverse.java
@@ -12,3 +12,15 @@ public class StringReverse {
         return str;
     }
 }
+class StringReverse2{
+    public static void main(String[] args) {
+        reverse("bich");
+    }
+    public static String reverse(String s){
+        char[] c = new char[s.length()];
+        for (int i = s.length() - 1; i >= 0; i--) {
+            System.out.print(s.charAt(i));
+        }
+        return s;
+    }
+}
