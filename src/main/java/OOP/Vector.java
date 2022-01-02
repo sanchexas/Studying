3fc48541ba542f50ea8616d2f428aefcf7ad1244
sergey-sanches-peres <sanchesgithub@mail.ru>
2022-01-02 21:06:32 +0300

    created class Vector

--- src/main/java/OOP/Vector.java
+++ src/main/java/OOP/Vector.java
@@ -1,12 +1,68 @@
 package OOP;
 
 public class Vector {
-    double x;
-    double y;
-    double z;
+    private double x;
+    private double y;
+    private double z;
 
-    Vector(){
+    public Vector(){
 
     }
 
+    public Vector(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public double calculateVectorLength(Vector vector){
+        double result;
+        result = Math.sqrt(x * x + y * y + z * z);
+        return result;
+    }
+
+    public double calculateScalar(Vector vector){
+        return x * vector.x + y * vector.y + z * vector.z;
+    }
+
+    @Override
+    public String toString() {
+        return "Vector{" +
+                "x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
+}
+class Try{
+    public static void main(String[] args) {
+        Vector vector = new Vector(23.2,12.3,11.4);
+        double scalar = vector.calculateScalar(vector);
+        double length = vector.calculateVectorLength(vector);
+        System.out.println("Скалярное произведение = " + scalar + "\n" + "Длина вектора = " + length);
+    }
 }
