d89f12b5544e7230f27029848bbe9b5a38618297
sergey-sanches-peres <sanchesgithub@mail.ru>
2022-01-02 05:08:18 +0300

    array training

--- src/main/java/ArraysStuff/task3.java
+++ src/main/java/ArraysStuff/task3.java
@@ -1,4 +1,21 @@
 package ArraysStuff;
 
 public class task3 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            //System.out.println(i);
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++) {
+               // System.out.print(j +" ");
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime){
+                System.out.print(i + " ");
+
+            }
+        }
+    }
 }
