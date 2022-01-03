6413872d1b0529ef4485659e054ff481479f197a
sergey-sanches-peres <sanchesgithub@mail.ru>
2022-01-03 16:44:23 +0300

    added Pyramid

--- src/main/java/OOP/Shapes/Pyramid.java
+++ src/main/java/OOP/Shapes/Pyramid.java
@@ -1,4 +1,13 @@
 package OOP.Shapes;
 
-public class Pyramid {
+public class Pyramid extends Shape{
+
+    private double height;
+    private double s;
+
+    public Pyramid(double height, double s) {
+        super(height * s * 4 / 3);
+        this.height = height;
+        this.s = s;
+    }
 }
