4ae97f0e5315395722fc1310cee38a04b12c9e86
sergey-sanches-peres <sanchesgithub@mail.ru>
2022-01-04 22:54:20 +0300

    selection sorting training

--- src/main/java/Algorithms/SelectionSorting.java
+++ src/main/java/Algorithms/SelectionSorting.java
@@ -29,3 +29,20 @@ public class SelectionSorting {
         return minEl;
     }
 }
+class SelectionSorting2{
+    public static void main(String[] args) {
+
+        int[] b = {21,23,54,23,6,23,7,34,65,8,12,43,2};
+        System.out.println(findMinElInSelectedRange(b , 7));
+
+    }
+    public static int findMinElInSelectedRange(int[] array, int startSearchingAt){
+        int minElement = array[startSearchingAt];
+        for (int i = startSearchingAt + 1; i < array.length; i++) {
+            if (array[i] < minElement ){
+                minElement = array[i];
+            }
+        }
+        return minElement;
+    }
+}
