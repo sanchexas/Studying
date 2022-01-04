3039bb3ac47686887d84f560476bec828f49811c
sergey-sanches-peres <sanchesgithub@mail.ru>
2022-01-04 20:29:15 +0300

    Algorithms training

--- src/main/java/Algorithms/FindMinElInArray.java
+++ src/main/java/Algorithms/FindMinElInArray.java
@@ -21,3 +21,21 @@ public class FindMinElInArray {
         System.out.println("Index of minimal value:" + minIndex);
     }
 }
+class FindMinElInArray2{
+    public static void main(String[] args) {
+
+        int[] myArray = {10,5,32,2,45,76,34};
+
+        int minElement = myArray[0];
+        int minIndex = 0;
+
+        for (int i = 1; i < myArray.length; i++) {
+            if (myArray[i] < minElement){
+                minElement = myArray[i];
+                minIndex = i;
+            }
+        }
+        System.out.println("min element is " + minElement);
+        System.out.println("min index is " + minIndex);
+    }
+}
