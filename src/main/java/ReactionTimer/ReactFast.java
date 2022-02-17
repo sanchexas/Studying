4e9a30fd777d22564b454d0ef89c626e140a575e
sergey-sanches-peres <sanchesgithub@mail.ru>
2022-02-17 23:48:05 +0300

    created Reaction Timer for fun

---
+++ src/main/java/ReactionTimer/ReactFast.java
@@ -0,0 +1,19 @@
+package ReactionTimer;
+
+import java.util.Scanner;
+
+public class ReactFast {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("3");
+        Thread.sleep(1000);
+        System.out.println("2");
+        Thread.sleep(1000);
+        System.out.println("1");
+        Thread.sleep(1000);
+
+        System.out.println("GO!");
+        long startTime = System.currentTimeMillis();
+
+        Scanner scanner = new Scanner(System.in)
+    }
+}
