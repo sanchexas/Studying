763f878843e2d00d700538474a80350b647ce2b8
sergey-sanches-peres <sanchesgithub@mail.ru>
2022-02-17 23:56:09 +0300

    created Class Encrypt

---
+++ src/main/java/EncryptionDecryption/Encrypt.java
@@ -0,0 +1,13 @@
+package EncryptionDecryption;
+
+public class Encrypt {
+    public static void main(String[] args) {
+
+        String text = "Hi, how are you?";
+        char[] chars = text.toCharArray();
+        for (char c :  chars){
+
+        }
+        System.out.println(text);
+    }
+}
