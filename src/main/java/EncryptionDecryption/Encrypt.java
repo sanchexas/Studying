c843b9724cb79ec6fc66d34031f105b428f9f369
sergey-sanches-peres <sanchesgithub@mail.ru>
2022-02-17 23:57:57 +0300

    added loop for

--- src/main/java/EncryptionDecryption/Encrypt.java
+++ src/main/java/EncryptionDecryption/Encrypt.java
@@ -6,7 +6,8 @@ public class Encrypt {
         String text = "Hi, how are you?";
         char[] chars = text.toCharArray();
         for (char c :  chars){
-
+            c+=5;
+            System.out.println(c);
         }
         System.out.println(text);
     }
