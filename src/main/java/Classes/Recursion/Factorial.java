887efca636b015f784e8c36daea7d9f303474d1a
sergey-sanches-peres <sanchesgithub@mail.ru>
2022-03-09 19:24:45 +0300

    recursion

---
+++ src/main/java/Classes/Recursion/Factorial.java
@@ -0,0 +1,20 @@
+package Classes.Recursion;
+
+public class Factorial {
+    int fact (int n){
+        int result;
+
+        if (n == 1) {
+
+            return 1;
+        }
+        result = fact(n-1) * n;
+        return result;
+    }
+}
+class Recursion{
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+        System.out.println(f.fact(5));
+    }
+}
